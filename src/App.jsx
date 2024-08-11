7c1aa8ba3e690ff589ae4f0ad53aeba1917ab58b
Brierre <heather.haighx@gmail.com>
2024-08-11 19:35:14 -0400

    reinstall bootstrap react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import ArmyBuilderApp from './ArmyBuilderApp';
 
